a4a3514e3c28c707d013ec17fe56b3b35b11f282
lpdft <772346730@qq.com>
2020-09-21 22:32:39 +0800

    [ruyue] feat:implement fetch

--- 5-async/3-fetch/index.js
+++ 5-async/3-fetch/index.js
@@ -1,6 +1,13 @@
 function fetchData(url) {
   // <-- start
   // TODO 23: 通过Fetch API实现异步请求
+  return fetch(url).then(responce => {
+    if (200 === responce.status) {
+      return responce.json();
+    } else {
+      Promise.reject(responce.status);
+    }
+  });
   // end -->
 }
 
