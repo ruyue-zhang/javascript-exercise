f86ce7df193558a1fda9d6c9a07e2cc6639f6527
lpdft <772346730@qq.com>
2020-09-21 20:56:26 +0800

    [ruyue] feat:use super introduce

--- 4-class/practice_4/worker.js
+++ 4-class/practice_4/worker.js
@@ -1,1 +1,13 @@
 // TODO 17: 在这里写实现代码
+import Person from './person';
+
+class Worker extends Person {
+  constructor(name, age) {
+    super(name, age);
+  }
+  introduce() {
+    return `${super.introduce()} I am a Worker. I have a job.`;
+  }
+}
+
+module.exports = Worker;
