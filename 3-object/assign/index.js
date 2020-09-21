c0c8d0e9ba0430266d30a6b7d5756531cbf5d8aa
lpdft <772346730@qq.com>
2020-09-21 21:28:54 +0800

    [ruyue] feat:implement addSerialNumber

--- 3-object/assign/index.js
+++ 3-object/assign/index.js
@@ -1,4 +1,8 @@
 export default function addSerialNumber(source) {
   // TODO 5: 在这里写实现代码，需要采用ES6 Object.assign
-  return source.assign();
+  const target = {
+    serialNumber: '12345',
+    properties: { color: 'green', status: 'processed' }
+  };
+  return Object.assign(source, target);
 }
