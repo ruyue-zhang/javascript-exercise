3af13a9de52d13f4270f4845306b204e464c2fac
lpdft <772346730@qq.com>
2020-09-21 21:13:24 +0800

    [ruyue] feat:judge class and output llass

--- 4-class/practice_5/teacher.js
+++ 4-class/practice_5/teacher.js
@@ -1,1 +1,18 @@
 // TODO 20: 在这里写实现代码
+import Person from './person';
+
+class Teacher extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+  introduce() {
+    if (this.klass) {
+      return `${super.introduce()} I am a Teacher. I teach Class ${this.klass}.`;
+    } else {
+      return `${super.introduce()} I am a Teacher. I teach No Class.`;
+    }
+  }
+}
+
+module.exports = Teacher;
