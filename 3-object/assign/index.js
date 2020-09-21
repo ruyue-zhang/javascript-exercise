3018573c0fddf1873a95f83a9b75f247ff334a6b
lpdft <772346730@qq.com>
2020-09-21 19:55:53 +0800

    [ruyue] feat:implement countTypesNumber

--- 3-object/assign/index.js
+++ 3-object/assign/index.js
@@ -1,3 +1,4 @@
 export default function addSerialNumber(source) {
   // TODO 5: 在这里写实现代码，需要采用ES6 Object.assign
+  return source.assign();
 }
--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,6 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  return Object.values(source)
+    .map(str => parseInt(str))
+    .reduce((result, curr) => result + curr);
 }
